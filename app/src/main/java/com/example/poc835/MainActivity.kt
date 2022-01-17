6d13fae6d865d9423d35e112fcea65ab9db1b17f
Matthew James <mjames4@woolworths.com.au>
2022-01-18 06:48:37 +1100

    Run Dart entry point on startup

--- app/src/main/java/com/example/poc835/MainActivity.kt
+++ app/src/main/java/com/example/poc835/MainActivity.kt
@@ -2,10 +2,21 @@ package com.example.poc835
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.embedding.engine.FlutterEngineCache
+import io.flutter.embedding.engine.dart.DartExecutor
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val flutterEngine = FlutterEngine(this)
+
+        flutterEngine.dartExecutor.executeDartEntrypoint(
+            DartExecutor.DartEntrypoint.createDefault()
+        )
+
+        FlutterEngineCache.getInstance().put("cached_engine", flutterEngine)
     }
 }
