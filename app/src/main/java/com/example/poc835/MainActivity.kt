81aae525c4df94a3e6fac5e46306048783102f00
Matthew James <mjames4@woolworths.com.au>
2022-01-19 07:10:35 +1100

    Continue trying to use a FlutterActivity

--- app/src/main/java/com/example/poc835/MainActivity.kt
+++ app/src/main/java/com/example/poc835/MainActivity.kt
@@ -2,6 +2,8 @@ package com.example.poc835
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 import io.flutter.embedding.android.FlutterActivity
 import io.flutter.embedding.android.FlutterActivityLaunchConfigs
 import io.flutter.embedding.engine.FlutterEngine
@@ -9,6 +11,10 @@ import io.flutter.embedding.engine.FlutterEngineCache
 import io.flutter.embedding.engine.dart.DartExecutor
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var incrementer: Button
+    private lateinit var counter: TextView
+    private var count = 0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -21,6 +27,15 @@ class MainActivity : AppCompatActivity() {
 
         FlutterEngineCache.getInstance().put("cached_engine", flutterEngine)
 
+        incrementer = findViewById(R.id.button)
+        counter = findViewById(R.id.counter)
+        counter.text = "0"
+
+        incrementer.setOnClickListener {
+            count++
+            counter.text = count.toString()
+        }
+
         startActivity(
             FlutterActivity
                 .withCachedEngine("cached_engine")
@@ -28,4 +43,5 @@ class MainActivity : AppCompatActivity() {
                 .build(this)
         )
     }
+
 }
